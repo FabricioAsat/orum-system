bb1f6ac0256acb8be63988cffdae3e8934f41d5f
Fabricio Asat <fabricioasat00@gmail.com>
2025-07-01 05:50:55 -0300

    init: start new orum system page design

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-background h-screen w-screen">
+      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+    </div>
+  );
+}
+
+export default App;
