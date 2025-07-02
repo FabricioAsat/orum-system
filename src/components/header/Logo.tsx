08070c145c067a6ffd91d48393f70e76cedbeafa
Fabricio Asat <fabricioasat00@gmail.com>
2025-07-02 09:39:50 -0300

    chore: logo functionality changed

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,14 +1,21 @@
 import logoIMG from "@assets/logo-orum.jpg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Logo = () => {
   return (
-    <Link to={"/"} className="h-full w-full md:w-auto">
-      <img
-        src={logoIMG}
-        alt="Logo Orum System"
-        className="h-full grayscale hover:grayscale-0 cursor-pointer max-h-10 md:max-h-16 mx-auto md:mx-0 md:py-2"
-      />
-    </Link>
+    <nav className="h-full w-full md:w-auto">
+      <NavLink
+        to={"/"}
+        className={({ isActive }) =>
+          `${isActive ? "grayscale-0" : "grayscale"}`
+        }
+      >
+        <img
+          src={logoIMG}
+          alt="Logo Orum System"
+          className="h-full cursor-pointer max-h-10 md:max-h-16 mx-auto md:mx-0 md:py-2 px-4"
+        />
+      </NavLink>
+    </nav>
   );
 };
